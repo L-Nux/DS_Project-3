80bf0f7820a0129d66637538baeb7ecd5172599a
Mykola <nikoly191298@gmail.com>
2021-11-20 11:23:23 +0100

    model for transport predictions, enhancement of dashboard

--- Dashboard/dashboard-app.py
+++ Dashboard/dashboard-app.py
@@ -1,40 +1,42 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
 import pickle
-from sklearn.ensemble import RandomForestClassifier
+
+import numpy as np
+import pandas as pd
+import streamlit as st
+from sklearn.preprocessing import OrdinalEncoder
 
 st.write("# Routes Prediction App")
 
 st.sidebar.header('User Input Features')
 
+# Reading the dataset
 routes_raw = pd.read_csv('data.csv')
 
+# Extracting only unique and sorted lists of ODs
+sources = routes_raw['sourcename'].sort_values().unique()
+targets = routes_raw['targetname'].sort_values().unique()
 
-
-
-sources = routes_raw['sourcename'].unique()
-targets = routes_raw['targetname'].unique()
-
+# Setting up the sidebar
 sourceName = st.sidebar.selectbox('Origin', sources)
 targetName = st.sidebar.selectbox('Destination', targets)
 totalPrice = st.sidebar.slider('Price', 0, 59, 362)
 totalNumberOfChanges = st.sidebar.slider('Number of changes', 0, 7)
 totalWalkingDistance = st.sidebar.slider('Walking distance', 0.0, 0.5, 0.96485)
 totalWaitingTime = st.sidebar.slider('Waiting time', 0, 6300, 76630)
-totalTravelTimeInSec = st.sidebar.slider('Travel time (seconds)', 424, 6300, 89987)
+totalTravelTimeInSec = st.sidebar.slider('Travel time', 0, 260)
 
 
+# Accepting the user input
 def user_input_features(sourceName, targetName, totalPrice, totalNumberOfChanges, totalWalkingDistance,
                         totalWaitingTime, totalTravelTimeInSec):
     data = {
-        'totalTravelTimeInSec': totalTravelTimeInSec,
-        'totalPrice': totalPrice,
-        'totalNumberOfChanges': totalNumberOfChanges,
-        'totalWalkingDistance': totalWalkingDistance,
-        'totalWaitingTime': totalWaitingTime,
-        'sourceName': sourceName,
-        'targetName': targetName}
+        'totaltraveltimeinsec': totalTravelTimeInSec,
+        'totalprice': totalPrice,
+        'totalnumberofchanges': totalNumberOfChanges,
+        'totalwalkingdistance': totalWalkingDistance,
+        'totalwaitingtime': totalWaitingTime,
+        'sourcename': sourceName,
+        'targetname': targetName}
     features = pd.DataFrame(data, index=[0])
     return features
 
@@ -44,8 +46,10 @@ def user_input_features(sourceName, targetName, totalPrice, totalNumberOfChanges
 #
 # st.write(df)
 
+
 st.write("### Press the button to get the recommended mode for your choice")
 
+# Predicting functionality
 if st.sidebar.button('Predict'):
 
     input_df = user_input_features(sourceName, targetName, totalPrice, totalNumberOfChanges, totalWalkingDistance,
@@ -53,11 +57,11 @@ if st.sidebar.button('Predict'):
 
     # Combines user input features with entire routes dataset
 
-    routes = routes_raw.drop(columns=['finiteautomaton'])
+    routes = routes_raw.drop(columns=['finalsolutionusedlabels'])
     df = pd.concat([input_df, routes], axis=0)
 
     # Encoding of ordinal features
-    encode = ['objective', 'consideredpreferences', 'sourcename', 'targetname', 'finalsolutionusedlabels']
+    encode = ['objective', 'consideredpreferences', 'sourcename', 'targetname', 'finiteautomaton']
     for col in encode:
         dummy = pd.get_dummies(df[col], prefix=col)
         df = pd.concat([df, dummy], axis=1)
@@ -68,12 +72,25 @@ if st.sidebar.button('Predict'):
     load_clf = pickle.load(open('routes_clf.pkl', 'rb'))
 
     # Apply model to make predictions
-    prediction = load_clf.predict(df)
+    prediction = load_clf.predict(df).astype(int)
     prediction_proba = load_clf.predict_proba(df)
 
     st.subheader('Prediction')
-    automata_pred = np.array(['all', 'ptOnly', 'noFlights'])
-    st.write(automata_pred[prediction])
+
+    # Extracting the mapped to the encoded values list of ODs
+    ord_enc = OrdinalEncoder()
+    routes_raw["finalsolutionusedlabels_code"] = ord_enc.fit_transform(routes_raw[["finalsolutionusedlabels"]])
+
+    ods_ordered_df = routes_raw.drop_duplicates(subset=['finalsolutionusedlabels', 'finalsolutionusedlabels_code'])
+    ods_ordered_df = ods_ordered_df[['finalsolutionusedlabels', 'finalsolutionusedlabels_code']]
+    ods_ordered_df.sort_values(by=['finalsolutionusedlabels_code'], inplace=True)
+    ods_ordered_lst = np.array(ods_ordered_df['finalsolutionusedlabels'])
+
+    # Outputing the prediction
+    st.write(ods_ordered_lst[prediction[0]])
+
+    # A bit of a celebration mood
+    st.balloons()
 
     st.subheader('Prediction Probability')
     st.write(prediction_proba)
--- Dashboard/dashboard-model-building.py
+++ Dashboard/dashboard-model-building.py
@@ -1,34 +1,44 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OrdinalEncoder
 
-routes = pd.read_csv('combined_csv_2dim.csv')
+routes = pd.read_csv('data.csv')
 
 # Ordinal feature encoding
 df = routes.copy()
 target = 'finalsolutionusedlabels'
-encode = ['objective', 'consideredPreferences', 'sourceName', 'targetName','finiteautomaton']
+encode = ['objective', 'consideredpreferences', 'sourcename', 'targetname', 'finiteautomaton']
 
 insight = df.describe()
 
 for col in encode:
     dummy = pd.get_dummies(df[col], prefix=col)
-    df = pd.concat([df,dummy], axis=1)
+    df = pd.concat([df, dummy], axis=1)
     del df[col]
 
-target_mapper = {'all':0, 'ptOnly':1, 'noFlights':2}
-def target_encode(val):
-    return target_mapper[val]
-
-df['finiteAutomaton'] = df['finiteAutomaton'].apply(target_encode)
+ord_enc = OrdinalEncoder()
+df["finalsolutionusedlabels_code"] = ord_enc.fit_transform(df[["finalsolutionusedlabels"]])
 
 # Separating X and y
-X = df.drop('finiteAutomaton', axis=1)
-Y = df['finiteAutomaton']
+X = df.drop(['finalsolutionusedlabels_code', 'finalsolutionusedlabels'], axis=1)
+Y = df['finalsolutionusedlabels_code']
+
+# Splitting the dataset
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3)
 
 # Build random forest model
 from sklearn.ensemble import RandomForestClassifier
-clf = RandomForestClassifier()
-clf.fit(X, Y)
+
+clf = RandomForestClassifier(n_estimators=100)
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+
+from sklearn import metrics
+
+# Checking the accuracy of the model
+print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
 
 # Saving the model
 import pickle
+
 pickle.dump(clf, open('routes_clf.pkl', 'wb'))
