c9402175b3dfa62abfa0bbe1384fcbfb9fc9f404
Mykola <nikoly191298@gmail.com>
2021-12-05 12:58:53 +0100

    implemented the best recommendation option based on survey and additional recommendation

--- Dashboard/streamlit-app.py
+++ Dashboard/streamlit-app.py
@@ -1,3 +1,4 @@
+import ast
 import pickle
 import re
 
@@ -7,10 +8,16 @@ from sklearn.preprocessing import OrdinalEncoder
 
 import plotly.express as px
 
+import operator
+
 from multipage import *
 
 # Config and setup
-st.set_page_config(layout="wide", page_title="Transport Recommendation Dashboard")
+st.set_page_config(layout="wide", page_title="Transport Recommendation Dashboard", menu_items={
+    'Get Help': 'https://plotly.com/python/parallel-coordinates-plot/',
+    'About': "# App for the route recommendation. \n"
+             "Version 0.1"
+})
 
 # References:
 # 1. Streamlit multipage framework
@@ -27,6 +34,14 @@ app.navbar_name = "Navigation"
 app.next_page_button = "Next Page"
 app.previous_page_button = "Previous Page"
 
+# Reading the dataset
+routes_raw = pd.read_csv('../data.csv')
+# routes_raw.rename({'sourcename': 'Source Name'}, axis=1, inplace=True)
+
+# Extracting only unique and sorted lists of ODs
+sources = routes_raw['sourcename'].sort_values().unique()
+targets = routes_raw['targetname'].sort_values().unique()
+
 
 def startpage():
     st.write("# Welcome :wave:")
@@ -37,133 +52,199 @@ def app1(prev_vars):  # First page
     # Questionnaire for the traveler to preconfigure filters
 
     with st.form("myform"):
-        st.write("What is the most important value for you in your itinerary?")
-        preference = st.selectbox('Filter type', ('Price', 'Waiting Time'))
-        save(var_list=[preference], name="Survey", page_names=["Dashboard"])
-        if st.form_submit_button("Submit"):
-            change_page(1)
+        st.write("### Survey to provide you with the best recommendation")
+        sourceName = st.selectbox('Origin', sources)
+        targetName = st.selectbox('Destination', targets, index=1)
+        # st.write("What is the most important value for you in your itinerary?")
+        # preference = st.selectbox('Filter type', ('Price', 'Waiting Time'))
+        travel_kids = st.selectbox('Do you travel with kids?', ('Yes', 'No'))
+
+        if sourceName != targetName:
+
+            if st.form_submit_button("Submit"):
+                # Saving variables to use them on the recommendation page
+                # save(var_list=[preference], name="Survey", page_names=["Dashboard"])
+                save(var_list=[survey_pref_calc(travel_kids), sourceName, targetName], name="Survey",
+                     page_names=["Dashboard"])
+                change_page(1)
+        else:
+            st.error(
+                'Recommendation cannot be done. Please select the destination that is different from the origin')
+
+
+preference = {
+    "totalprice": 0,
+    "totalwaitingtime": 0,
+    "totaltraveltime": 0,
+    "totalwalkingdistance": 0,
+    "numberoftransfers": 0
+}
+
+
+# Based on survey preference calculation
+def survey_pref_calc(travel_kids):
+    if travel_kids == "Yes":
+        preference["totalprice"] += 0.5
+        preference["totalwaitingtime"] += 0.9
+        preference["totaltraveltime"] += 0.9
+        preference["totalwalkingdistance"] += 1
+        preference["numberoftransfers"] += 1
+    else:
+        preference["totalprice"] += 0.7
+        preference["totalwaitingtime"] += 0.5
+        preference["totaltraveltime"] += 0.5
+        preference["totalwalkingdistance"] += 0.5
+        preference["numberoftransfers"] += 0.5
+
+    return max(preference.items(), key=operator.itemgetter(1))[0]
 
 
 # Recommendation page
 def app2(prev_vars):  # Second page
     st.write("# Transport Recommendation App")
 
-    st.header('User Input Features')
+    preference = prev_vars[0]
+    sourceName = prev_vars[1]
+    targetName = prev_vars[2]
 
-    # Reading the dataset
-    routes_raw = pd.read_csv('../data.csv')
+    chosenODs = routes_raw.loc[
+        (routes_raw["sourcename"] == sourceName) & (routes_raw.targetname == targetName)
+        ]
 
-    # Extracting only unique and sorted lists of ODs
-    sources = routes_raw['sourcename'].sort_values().unique()
-    targets = routes_raw['targetname'].sort_values().unique()
+# Checking if survey was filled in
+    if preference:
 
-    st.write("#### Configure filters and press the button to get the recommended mode for your choice")
-    if prev_vars == 'Price':
-        price_default = 0
-        waiting_time_default = 1.0
+        best_recommendation_df =  chosenODs[(chosenODs[preference] == chosenODs[preference].min())].head(1)
+
     else:
-        waiting_time_default = 0.0
-        price_default = 20
-
-    # Mechanism for additional routes propositions
-    # Vars for additional route propositions
-    step_number_changes = 1
-    step_walking_distance = 50
-    step_waiting_time = 0.5
-    step_price = 10
-    step_travel_time = 0.5
-
-    # Setting up filters
-    sourceName = st.selectbox('Origin', sources)
-    targetName = st.selectbox('Destination', targets, index=1)
-
-    totalPrice = st.slider('Price (Euro)', 1, 59, (0, 59))
-    totalNumberOfChanges = st.slider('Number of changes', 0, 7, (0, 7))
-    totalWalkingDistance = st.slider('Walking distance (m)', 0, 965, (0, 965))
-    totalWaitingTime = st.slider('Waiting time (h)', 0.0, 3.5, (0.0, 3.5), step=0.5)
-    totalTravelTimeInSec = st.slider('Travel time (h)', 0.5, 4.5, (0.0, 4.5), step=0.5)
-
-    # Accepting the user input
-    def user_input_features(sourceName, targetName, totalPrice, totalNumberOfChanges, totalWalkingDistance,
-                            totalWaitingTime, totalTravelTimeInSec):
-        data = {
-            'totaltraveltimeinsec': totalTravelTimeInSec,
-            'totalprice': totalPrice,
-            'totalnumberofchanges': totalNumberOfChanges,
-            'totalwalkingdistance': totalWalkingDistance,
-            'totalwaitingtime': totalWaitingTime,
-            'sourcename': sourceName,
-            'targetname': targetName}
-        features = pd.DataFrame(data, index=[0])
-        return features
-
-    # # Displays the user input features
-    # st.subheader('User Input features')
-    #
-    # st.write(df)
-
-    # Recommending functionality
-    if st.button('Recommend'):
-        # form.empty()
-        with st.spinner('Processing...'):
-
-            if sourceName != targetName:
-
-                # input_df = user_input_features(sourceName, targetName, totalPrice, totalNumberOfChanges,
-                #                                totalWalkingDistance,
-                #                                totalWaitingTime, totalTravelTimeInSec)
-
-                st.subheader('Recommendation')
-
-                chosenODs = routes_raw.loc[(routes_raw.sourcename == sourceName) & (routes_raw.targetname == targetName)
-                                           & (routes_raw.totalprice >= totalPrice[0]) & (
-                                                   routes_raw.totalprice <= totalPrice[1])
-                                           & (routes_raw.totalwalkingdistance >= totalWalkingDistance[0]) & (
-                                                   routes_raw.totalwalkingdistance <= totalWalkingDistance[1])
-                                           & (routes_raw.totalnumberofchanges >= totalNumberOfChanges[0]) & (
-                                                   routes_raw.totalnumberofchanges <= totalNumberOfChanges[1])
-                                           & (routes_raw.totaltraveltimeinsec >= totalTravelTimeInSec[0]) & (
-                                                   routes_raw.totaltraveltimeinsec <= totalTravelTimeInSec[1])
-                                           & (routes_raw.totalwaitingtime >= totalWaitingTime[0]) & (
-                                                   routes_raw.totalwaitingtime <= totalWaitingTime[1])
-                                           ]
-
-                chosenODs.reset_index(drop=True, inplace=True)
-
-                chosenODs = chosenODs.assign(
-                    id=(chosenODs['totalprice'].astype(str) + '_' + chosenODs['totalwalkingdistance'].astype(str)
-                        + '_' + chosenODs['totalnumberofchanges'].astype(str)
-                        + '_' + chosenODs['totaltraveltimeinsec'].astype(str)
-                        + '_' + chosenODs['totalwaitingtime'].astype(str))
-
-                    .astype('category').cat.codes)
-
-                chosenODs['id'] = chosenODs['id'].astype(np.int64)
+        return "### Please fill in the survey to get the best recommendation based on our algorithm"
 
 
-                st.write(chosenODs)
-                st.write(totalPrice)
+# Function for generating additional recommendations
+    def additional_recommendation(df, preference):
+        minvalue = df[preference].min()
 
-                # final_solutions = chosenODs['finalsolutionusedlabels']
+        threshold = 0.25
 
-                # Make the transport labels look user-friendly
-                clean_recommendation = []
+        additional_recommendation_df = df.loc[(df[preference] > minvalue) & (df[preference] <= (minvalue + threshold))]
 
-                # for el in final_solutions:
-                #
-                #     clean_recommendation.append(re.sub(r"[\[\]]", "", el))
-                #
-                # # Output the recommendation
-                # for el in clean_recommendation:
-                #
-                #     st.write(el)
+        while additional_recommendation_df.empty:
 
-                # st.write('Do you want to see adjusted propositions?')
-                # if st.button('Yes!'):
-                #     st.write('Choose the filter to adjust')
-                #     st.selectbox('Filter type', ('Price, Waiting Time'))
+            threshold += 0.25
+            additional_recommendation_df = df.loc[(df[preference] > minvalue) & (df[preference] <= (minvalue + threshold))]
+            if not additional_recommendation_df.empty:
+                additional_recommendation_df.drop_duplicates(subset=["finalsolutionusedlabels"], inplace = True)
+                st.write(f"If you increase __{preference}__ by __{threshold}__ units you can consider taking next options:")
+                st.write(additional_recommendation_df["finalsolutionusedlabels"].to_string(index=False))
+                break
+        return additional_recommendation_df
 
-                with st.expander('Explanation of your recommendation'):
+    if not best_recommendation_df.empty:
+        st.write(best_recommendation_df["sourcename"].to_string(index=False))
+        additional_recommendation_df = additional_recommendation(chosenODs, preference)
+
+
+        comparison_best_additional = pd.concat([best_recommendation_df, additional_recommendation_df], axis=0)
+
+        comparison_best_additional.reset_index(drop=True, inplace=True)
+
+        comparison_best_additional = comparison_best_additional.assign(
+            id=(comparison_best_additional['totalprice'].astype(str) + '_' + comparison_best_additional['totalwalkingdistance'].astype(str)
+                + '_' + comparison_best_additional['totalnumberofchanges'].astype(str)
+                + '_' + comparison_best_additional['totaltraveltimeinsec'].astype(str)
+                + '_' + comparison_best_additional['totalwaitingtime'].astype(str))
+
+                .astype('category').cat.codes)
+
+        comparison_best_additional['id'] = comparison_best_additional['id'].astype(np.int64)
+
+        comparison_best_additional_fig = px.parallel_coordinates(
+            comparison_best_additional,
+            color="id",
+            labels={"totalprice": "Price",
+                    "totalwalkingdistance": "Walking Distance", "totalnumberofchanges": "Number of Changes",
+                    "totaltraveltimeinsec": "Travel Time", "totalwaitingtime": "Waiting Time",
+                    "id": "Route"},
+        )
+
+        st.plotly_chart(comparison_best_additional_fig, use_container_width=True)
+
+
+    else:
+        st.warning("Unfortunately, there is no recommendation for the chosen itinerary. Please select another one")
+
+
+
+# Manual filtering section
+    with st.expander('Manual filtering'):
+
+        st.write("#### Configure filters and press the button to get the recommended mode for your choice")
+
+        # Setting up filters
+        sourceName = st.selectbox('Origin', sources)
+        targetName = st.selectbox('Destination', targets, index=1)
+
+        totalPrice = st.slider('Price (Euro)', 1, 59, (0, 59))
+        totalNumberOfChanges = st.slider('Number of changes', 0, 7, (0, 7))
+        totalWalkingDistance = st.slider('Walking distance (m)', 0, 965, (0, 965))
+        totalWaitingTime = st.slider('Waiting time (h)', 0.0, 3.5, (0.0, 3.5), step=0.5)
+        totalTravelTimeInSec = st.slider('Travel time (h)', 0.5, 4.5, (0.0, 4.5), step=0.5)
+
+        # Recommending functionality
+        if st.button('Recommend'):
+            with st.spinner('Processing...'):
+
+                if sourceName != targetName:
+
+                    st.subheader('Recommendation')
+
+                    chosenODs = routes_raw.loc[
+                        (routes_raw["sourcename"] == sourceName) & (routes_raw.targetname == targetName)
+                        & (routes_raw.totalprice >= totalPrice[0]) & (
+                                routes_raw.totalprice <= totalPrice[1])
+                        & (routes_raw.totalwalkingdistance >= totalWalkingDistance[0]) & (
+                                routes_raw.totalwalkingdistance <= totalWalkingDistance[1])
+                        & (routes_raw.totalnumberofchanges >= totalNumberOfChanges[0]) & (
+                                routes_raw.totalnumberofchanges <= totalNumberOfChanges[1])
+                        & (routes_raw.totaltraveltimeinsec >= totalTravelTimeInSec[0]) & (
+                                routes_raw.totaltraveltimeinsec <= totalTravelTimeInSec[1])
+                        & (routes_raw.totalwaitingtime >= totalWaitingTime[0]) & (
+                                routes_raw.totalwaitingtime <= totalWaitingTime[1])
+                        ]
+
+                    chosenODs.reset_index(drop=True, inplace=True)
+
+                    chosenODs = chosenODs.assign(
+                        id=(chosenODs['totalprice'].astype(str) + '_' + chosenODs['totalwalkingdistance'].astype(str)
+                            + '_' + chosenODs['totalnumberofchanges'].astype(str)
+                            + '_' + chosenODs['totaltraveltimeinsec'].astype(str)
+                            + '_' + chosenODs['totalwaitingtime'].astype(str))
+
+                            .astype('category').cat.codes)
+
+                    chosenODs['id'] = chosenODs['id'].astype(np.int64)
+
+                    st.write(chosenODs)
+                    st.write(totalPrice)
+
+                    # final_solutions = chosenODs['finalsolutionusedlabels']
+
+                    # Make the transport labels look user-friendly
+                    clean_recommendation = []
+
+                    # for el in final_solutions:
+                    #
+                    #     clean_recommendation.append(re.sub(r"[\[\]]", "", el))
+                    #
+                    # # Output the recommendation
+                    # for el in clean_recommendation:
+                    #
+                    #     st.write(el)
+
+                    st.write('### Explanation of your recommendation')
+                    st.info("* Drag the lines along the axes to filter regions.\n"
+                            "* Double click on the axes releases selection.\n"
+                            "* Drag different attributes for better comparison of choices.\n")
                     fig = px.parallel_coordinates(
                         chosenODs,
                         color="id",
@@ -179,15 +260,9 @@ def app2(prev_vars):  # Second page
                     if (len(chosenODs.index) > friendly_amount_lines):
                         st.info("Looks complicated? Please try to filter your preferences a bit more.")
 
-                    st.write(
-                        '- If you *increase waiting time for 1h* then it will be better to take **bla bla car** and then **flixbus**\n'
-                        '- If you *increase traveling time time for 2h* then it will be better to take **train** and then **flixbus**')
-
-
-
-            else:
-                st.error(
-                    'Recommendation cannot be done. Please select the destination that is different from the origin')
+                else:
+                    st.error(
+                        'Recommendation cannot be done. Please select the destination that is different from the origin')
 
 
 # Page for the transport mode prediction using random forest model
@@ -196,13 +271,6 @@ def app3(prev_vars):  # Third page
     st.write("# Transport Prediction")
 
     st.header('User Input Features')
-
-    # Reading the dataset
-    routes_raw = pd.read_csv('../data.csv')
-
-    # Extracting only unique and sorted lists of ODs
-    sources = routes_raw['sourcename'].sort_values().unique()
-    targets = routes_raw['targetname'].sort_values().unique()
 
     st.write("#### Configure filters and press the button to get the recommended mode for your choice")
 
