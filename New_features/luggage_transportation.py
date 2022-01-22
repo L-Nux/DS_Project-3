f03dbf9ff230417e361c836c4bab28a5cd906a9a
Marina Kozhevnikova <85805868+marinakoj@users.noreply.github.com>
2022-01-22 13:46:34 +0100

    new categorical feature with luggage policy checks

--- New_features/luggage_transportation.py
+++ New_features/luggage_transportation.py
@@ -7,6 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/16Et31XDnwuW-LMXNVB4rCAAJoHOpwVbE
 """
 
+
 import pandas as pd #data processing
 
 file='finalnew.csv' #load the current dataset 
